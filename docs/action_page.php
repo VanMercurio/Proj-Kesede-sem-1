bb72c58f843556cefff2311a380644075b01945d
Vanessa Mercurio <142271576+VanMercurio@users.noreply.github.com>
2023-11-21 16:53:58 -0300

    Correção na imagem responsiva

--- docs/action_page.php
+++ docs/action_page.php
@@ -12,15 +12,15 @@ if ($conn->connect_error) {
 }
 
 // Coleta os dados do formulário
-$nome = $_POST['name'];
-$endereco = $_POST['adress'];
-$cidade = $_POST['city'];
-$data_nascimento = $_POST['nasc'];
-$email = $_POST['email'];
-$ponto_referencia = $_POST['pontoref'];
-$senha = $_POST['password'];
-$sexo = $_POST['sexo'];
-$classificacao = implode(', ', $_POST['adicionais']); // Selecione todos os adicionais selecionados
+$nome = $_REQUEST['name'];
+$endereco = $_REQUEST['adress'];
+$cidade = $_REQUEST['city'];
+$data_nascimento = $_REQUEST['nasc'];
+$email = $_REQUEST['email'];
+$ponto_referencia = $_REQUEST['pontoref'];
+$senha = $_REQUEST['password'];
+$sexo = $_REQUEST['sexo'];
+$classificacao = implode(', ', $_REQUEST['adicionais']); // Selecione todos os adicionais selecionados
 
 // Inserir os dados no banco de dados
 $sql = "INSERT INTO usuarios (nome, endereco, cidade, data_nascimento, email, ponto_referencia, senha, sexo, classificacao) VALUES ('$nome', '$endereco', '$cidade', '$data_nascimento', '$email', '$ponto_referencia', '$senha', '$sexo', '$classificacao')";
@@ -32,4 +32,3 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $conn->close();
-?>
